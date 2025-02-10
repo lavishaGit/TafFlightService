c1cd4c1975f97e5445f644d974ac654ee971d23d
lavishaGit <97263933+lavishaGit@users.noreply.github.com>
2025-02-10 10:16:35 -0800

    Update FlightController.java

--- src/main/java/com/tfa/flights/Controller/FlightController.java
+++ src/main/java/com/tfa/flights/Controller/FlightController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+     @CrossOrigin(origins = "*")
 @RequestMapping("/flights")
 public class FlightController {
     private static final Logger logger = LogManager.getLogger(FlightController.class);
